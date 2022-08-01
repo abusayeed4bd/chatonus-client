dc38366cb9553d65b684cc4ca50c5046c41f5c3d
abusayeed4bd <abusayeed4bd@gmail.com>
2022-08-01 13:35:13 +0600

    socket context completed

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, TextField, Grid, Typography, Container, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
+import { useContext } from 'react';
+import { SocketContext } from './SocketContext';
 
 const Options = ({ children }) => {
+    const { me, callAccepted, name, setName, leaveCall, callUser, callEnded } = useContext(SocketContext);
+    const idToCall = useState('');
     return (
         <div>
             Options
